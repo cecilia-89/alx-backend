ed710be557e3cbd647bdfd30c8d5bfb1e4f0a56c
cecilia-89 <catabong89@gmail.com>
2023-01-25 06:37:20 +0100

    added readme

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,11 +16,11 @@ class FIFOCache(BaseCaching):
         if key is not None or item is not None:
             self.cache_data[key] = item
 
-        length = len(self.cache_data.keys())
-        if length > BaseCaching.MAX_ITEMS:
-            discard = list(self.cache_data.keys())[0]
-            del self.cache_data[discard]
-            print("DISCARD: {}\n".format(discard))
+            length = len(self.cache_data.keys())
+            if length > BaseCaching.MAX_ITEMS:
+                discard = list(self.cache_data.keys())[0]
+                del self.cache_data[discard]
+                print("DISCARD: {}\n".format(discard))
 
     def get(self, key):
         """returns a value"""
