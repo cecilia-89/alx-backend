7ae992222a1c2c37142815e253b0b0983b09209c
cecilia-89 <catabong89@gmail.com>
2023-01-25 06:40:35 +0100

    added readme

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -18,5 +18,7 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """returns a value"""
+        if key is None:
+            return None
         value = self.cache_data.get(key)
         return value
