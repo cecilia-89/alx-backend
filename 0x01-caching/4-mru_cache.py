06d9427863e767a1828897544300f5ff4458d603
cecilia-89 <catabong89@gmail.com>
2023-01-25 22:45:54 +0100

    mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -4,25 +4,34 @@
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class MRUCache(BaseCaching):
     """basic caching system"""
+    count = 0
 
     def __init__(self):
         """instantiates an object"""
         super().__init__()
+        self.items = {}
 
     def put(self, key, item):
         """assigns item to the cache system"""
         if key is None or item is None:
             return
+        MRUCache.count += 1
+        self.items[key] = MRUCache.count
         self.cache_data[key] = item
         length = len(self.cache_data.keys())
         if length > BaseCaching.MAX_ITEMS:
-            discard = list(self.cache_data.keys())[-1]
-            del self.cache_data[discard]
-            print("DISCARD: {}\n".format(discard))
+            most = max(self.items.values())
+            value = [k for k, v in self.items.items() if v == most]
+            del self.cache_data[value[0]]
+            del self.items[value[0]]
+            print("DISCARD: {}".format(value[0]))
 
     def get(self, key):
         """returns a value"""
+        if key in self.items.keys():
+            MRUCache.count += 1
+            self.items[key] = MRUCache.count
         value = self.cache_data.get(key)
         return value
