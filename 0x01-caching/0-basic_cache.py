c88be0dc62de26b021a292a9c4bad69d9d76419e
cecilia-89 <catabong89@gmail.com>
2023-01-25 06:11:45 +0100

    initial commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module: 0-basic_cache"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """basic caching system"""
+
+    def __init__(self):
+        """instantiates an object"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assigns item to the cache system"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """returns a value"""
+        value = self.cache_data.get(key)
+        return value
