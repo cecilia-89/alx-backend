5e3491918600918e8dbcf19799c8a884431f2fc1
cecilia-89 <catabong89@gmail.com>
2023-01-25 17:57:28 +0100

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -26,16 +26,3 @@ class FIFOCache(BaseCaching):
         """returns a value"""
         value = self.cache_data.get(key)
         return value
-
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
