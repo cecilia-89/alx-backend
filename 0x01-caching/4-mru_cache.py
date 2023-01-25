346f0404a5d63b6710f678689c0984d779f6b3f6
cecilia-89 <catabong89@gmail.com>
2023-01-25 23:20:22 +0100

    mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -12,6 +12,7 @@ class MRUCache(BaseCaching):
         """instantiates an object"""
         super().__init__()
         self.items = {}
+        self.prevMax = ""
 
     def put(self, key, item):
         """assigns item to the cache system"""
@@ -22,16 +23,17 @@ class MRUCache(BaseCaching):
         self.cache_data[key] = item
         length = len(self.cache_data.keys())
         if length > BaseCaching.MAX_ITEMS:
-            most = max(self.items.values())
-            value = [k for k, v in self.items.items() if v == most]
+            value = [k for k, v in self.items.items() if v == self.prevMax]
             del self.cache_data[value[0]]
             del self.items[value[0]]
             print("DISCARD: {}".format(value[0]))
+        self.prevMax = max(self.items.values())
 
     def get(self, key):
         """returns a value"""
         if key in self.items.keys():
             MRUCache.count += 1
             self.items[key] = MRUCache.count
+            self.prevMax = self.items.get(key)
         value = self.cache_data.get(key)
         return value
