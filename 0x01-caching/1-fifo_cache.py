7455cfb8fa93d76cea817efc4c5fdc530a09cd9d
cecilia-89 <catabong89@gmail.com>
2023-01-25 17:56:52 +0100

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,11 +18,24 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
         length = len(self.cache_data.keys())
         if length > BaseCaching.MAX_ITEMS:
-            first_key = list(self.cache_data.keys())[0]
-            del self.cache_data[first_key]
-            print("DISCARD: {}\n".format(first_key))
+            firstIn = list(self.cache_data.keys())[0]
+            del self.cache_data[firstIn]
+            print("DISCARD: {}".format(firstIn))
 
     def get(self, key):
         """returns a value"""
         value = self.cache_data.get(key)
         return value
+
+my_cache = FIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
