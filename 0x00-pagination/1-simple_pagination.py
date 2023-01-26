ddd31b0342d6939b93f49ba37215fc50c36f7d87
cecilia-89 <catabong89@gmail.com>
2023-01-26 15:56:55 +0100

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,4 +38,8 @@ class Server:
         for item in [page, page_size]:
             assert isinstance(item, int) and item > 0
         index = index_range(page, page_size)
+        for i in index:
+            if i > len(self.__dataset):
+                return '[]'
+
         return self.__dataset[index[0]: index[-1]]
