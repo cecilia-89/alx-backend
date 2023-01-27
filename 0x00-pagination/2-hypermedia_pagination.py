423031570ea784003fb8cb0f33afb102f5f30f98
cecilia-89 <catabong89@gmail.com>
2023-01-27 13:50:05 +0100

    pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Module: 0-simple_helper_func"""
+from typing import Tuple, List, Dict
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """returns list paginator params"""
+    end_index = page * page_size
+    start_index = end_index - page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """instantiates an object"""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns data set with appropriate page"""
+        for item in [page, page_size]:
+            assert isinstance(item, int) and item > 0
+        self.dataset()
+        index = index_range(page, page_size)
+        return self.__dataset[index[0]: index[-1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        page_data = self.get_page(page, page_size)
+        prev_page, next_page = page - 1, page + 1
+        if prev_page <= 0:
+            prev_page = None
+
+        if len(page_data) == 0:
+            next_page = None
+
+        page_dict = {"page_size": len(page_data), "page": page,
+                     "data": page_data, "next_page": next_page,
+                     "prev_page": prev_page,
+                     "total_page": math.ceil(len(self.__dataset) / page_size)}
+
+        return page_dict
