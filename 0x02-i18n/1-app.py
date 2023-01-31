3e0e3ae4e8d005fd56b5d856012ea7cbd0aed773
cecilia-89 <catabong89@gmail.com>
2023-01-31 12:54:16 +0100

    i8tn

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Sets up a basic flask app"""
 
+from flask import request
 from flask_babel import Babel
 app = __import__('0-app').app
 
 babel = Babel(app)
 
-class Class:
+class Config:
     """configures other languages in our app"""
     LANGUAGES = ["en", "fr"]
+
