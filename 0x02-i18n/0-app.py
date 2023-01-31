704dbd6aa60690c9d5d8ecad05033dcdfec8087b
cecilia-89 <catabong89@gmail.com>
2023-01-31 12:35:59 +0100

    i8tn

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def main():
+def main() -> None:
     """creates a home route"""
     return render_template('0-index.html')
-
 
 if __name__ == "__main__":
     app.run()
