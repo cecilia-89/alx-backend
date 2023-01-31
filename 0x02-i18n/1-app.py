f42ce4d8eb516d16e042ffa6d908bd7877ee5042
cecilia-89 <catabong89@gmail.com>
2023-01-31 13:14:47 +0100

    i8tn

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 """Sets up a basic flask app"""
 
+from flask import Flask, render_template
 from flask_babel import Babel
-app = __import__('0-app').app
+
+
+app = Flask(__name__)
 babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """creates a home route"""
+    return render_template('0-index.html')
 
 
 class Config:
     """configures other languages in our app"""
     LANGUAGES = ["en", "fr"]
+
+
+if __name__ == "__main__":
+    app.run()
