572c1fd9deff8617074b6dc631288cc31742c5bd
cecilia-89 <catabong89@gmail.com>
2023-02-03 18:40:42 +0100

    i18n

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -23,6 +23,7 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
+@babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
     has_locale = request.args.to_dict().get("locale")
