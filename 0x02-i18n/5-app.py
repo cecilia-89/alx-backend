5313b4128b00e57cbf4479551ced8d980a20bec4
cecilia-89 <catabong89@gmail.com>
2023-02-03 18:47:14 +0100

    i18n

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -17,6 +17,8 @@ babel = Babel(app)
 class Config:
     """configures other languages in our app"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
