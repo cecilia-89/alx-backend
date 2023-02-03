0cbe381041edf9dd2422cf3865c92f05aa7328df
cecilia-89 <catabong89@gmail.com>
2023-02-03 08:33:05 +0100

    i8tn

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,18 +2,19 @@
 """Module: 2-app.py"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 
 
 class Config:
     """configures other languages in our app"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
+
+@babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
@@ -21,9 +22,9 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """creates a home route"""
+
     return render_template('3-index.html')
 
-babel = Babel(app, locale_selector=get_locale)
 
 if __name__ == "__main__":
     app.run()
