532df9d7ee9f25486ecc366a1bedbaa5479581e5
cecilia-89 <catabong89@gmail.com>
2023-02-03 20:17:20 +0100

    i18n

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""Module: 5-app.py"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """configures other languages in our app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages"""
+    has_locale = request.args.to_dict().get("locale")
+    if has_locale in app.config['LANGUAGES']:
+        return has_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """determines the timezone"""
+
+    has_timezone = request.args.to_dict()
+    user_timezone = g.user
+
+    for i in [has_timezone, user_timezone]:
+        try:
+            time_zone = i.get('timezone')
+            pytz.timezone(time_zone).zone
+            return time_zone
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """creates a home route"""
+    return render_template('7-index.html')
+
+
+def get_user():
+    """returns a logged in user"""
+    has_user = request.args.to_dict().get("login_as")
+    if has_user is not None:
+        return users.get(int(has_user))
+
+
+@app.before_request
+def before_request():
+    """executes before all other functions"""
+    user = get_user()
+    g.user = user
+
+
+if __name__ == "__main__":
+    app.run()
