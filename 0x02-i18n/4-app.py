f51372f2be72937725b9e1004574e15f8a203d8a
cecilia-89 <catabong89@gmail.com>
2023-02-03 14:02:30 +0100

    i8tn

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,6 +20,9 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
+    has_locale = request.args.to_dict().get("locale")
+    if has_locale in app.config['LANGUAGES']:
+        return has_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
